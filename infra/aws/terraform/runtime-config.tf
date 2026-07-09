cdc4070d48d0f7600f8147b9947e827b7ce8c186
시원 <96858320+NearthYou@users.noreply.github.com>
2026-07-10 03:59:08 +0900

    Refactor: ECS Secret local 중복 제거

--- infra/aws/terraform/runtime-config.tf
+++ infra/aws/terraform/runtime-config.tf
@@ -1,17 +1,4 @@
 locals {
-  ecs_api_secret_names = toset([
-    "AUTH_TOKEN_SECRET",
-    "CLOUDFORMATION_TEMPLATE_TOKEN_SECRET",
-    "DATABASE_URL",
-    "GIT_APP_PRIVATE_KEY_BASE64",
-    "GIT_APP_STATE_SECRET",
-    "GIT_OAUTH_CLIENT_SECRET",
-    "KAKAO_OAUTH_CLIENT_SECRET",
-    "NAVER_OAUTH_CLIENT_SECRET",
-    "OPENAI_API_KEY",
-    "REDIS_URL"
-  ])
-
   ecs_api_secrets_manager_names = toset([
     "DATABASE_URL",
     "GIT_APP_PRIVATE_KEY_BASE64",
@@ -27,4 +14,9 @@ locals {
     "GIT_APP_STATE_SECRET",
     "REDIS_URL"
   ])
+
+  ecs_api_secret_names = setunion(
+    local.ecs_api_secrets_manager_names,
+    local.ecs_api_ssm_secure_string_names
+  )
 }
